5b8aee41a7b6183681361aa446e65602aff28fcb
Arijit Nukala <88689186+tikicode@users.noreply.github.com>
2023-12-05 15:48:00 -0500

    Finish train code

--- nllb.py
+++ nllb.py
@@ -1,7 +1,8 @@
 import argparse
 import logging
 import random
-from transformers import AutoTokenizer, AutoModel, AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer, AutoModel, AutoModelForSeq2SeqLM, NllbTokenizerFast, get_constant_schedule_with_warmup
+from transformers.optimization import Adafactor
 import torch
 import torch.nn as nn
 import numpy as np
@@ -29,6 +30,23 @@ def clear_mem():
   gc.collect()
   torch.cuda.empty_cache()
   
+def plot_loss_and_bleu(losses, bleu_scores, iters, save_path):
+  plt.plot(losses)
+  plt.xlabel("Iterations")
+  plt.ylabel("Loss")
+  plt.title(str(iters)+" Iterations")
+  plt.show()
+  plt.savefig(save_path+'/train_loss.png')
+  
+  plt.plot(bleu_scores)
+  plt.xlabel("Iterations")
+  plt.ylabel("Bleu Score on Test")
+  plt.title(str(iters)+" Iterations")
+  plt.show()
+  plt.savefig(save_path+'/bleu_score.png')
+  
+def sort_dataset_by_size(data):
+    return data.sort_values(by=['한국어_어절수'])
 
 def main():
     ap = argparse.ArgumentParser()
@@ -42,80 +60,92 @@ def main():
                     help='print loss info every this many training examples')
     ap.add_argument('--checkpoint_every', default=1000, type=int,
                     help='write out checkpoint every this many training examples')
+    ap.add_argument('--plot_every', default=1000, type=int,
+                    help='plot this many training examples')
     ap.add_argument('--initial_learning_rate', default=0.001, type=float,
                     help='initial learning rate')
-    ap.add_argument('--src_lang', default='fr',
-                    help='Source (input) language code, e.g. "fr"')
-    ap.add_argument('--tgt_lang', default='en',
-                    help='Source (input) language code, e.g. "en"')
-    ap.add_argument('--train_path', default='en_kr_data/train/ko2en_training_csv/ko2en_finance_1_training.csv',
+    ap.add_argument('--train_path', default='en_kr_data/train/ko2en_training_csv/ko2en_medical_1_training.csv',
                     help='training file')
-    ap.add_argument('--val_path', default='en_kr_data/val/ko2en_validation_csv/ko2en_finance_2_validation.csv',
+    ap.add_argument('--val_path', default='en_kr_data/val/ko2en_validation_csv/ko2en_medical_2_validation.csv',
                     help='val file')
     ap.add_argument('--out_file', default='out.txt',
                     help='output file')
-    ap.add_argument('--save_path', default='/models',
+    ap.add_argument('--model_save_path', default='/models',
                     help='folder to save models')
-    ap.add_argument('--load_checkpoint', nargs=1,
+    ap.add_argument('--plot_save_path', default='/plots',
+                    help='folder to save plots')
+    ap.add_argument('--load_checkpoint', default=None,
                     help='checkpoint file to start from')
+    ap.add_argument('--curriculum', '-c', action='store_true', 
+                    help='curriculum learning (process data by size)')
+    ap.add_argument('--lr', default=1e-4,
+                    help='learning rate')
+    
 
     args = ap.parse_args()
 
-
     train_df = pd.read_csv(args.train_path, sep=',')
+    train_df = sort_dataset_by_size(train_df)
+
     val_test_df = pd.read_csv(args.val_path, sep=',')
     val_df, test_df = val_test_df[:len(val_test_df)//2], val_test_df[len(val_test_df)//2:]
 
     if args.load_checkpoint is not None:
-        pass
+        tokenizer = NllbTokenizerFast.from_pretrained(args.load_checkpoint)
+        nllb = AutoModelForSeq2SeqLM.from_pretrained(args.load_checkpoint)
     else:
         name = "facebook/nllb-200-distilled-600M"
-        tokenizer = AutoTokenizer.from_pretrained(name)
+        tokenizer = NllbTokenizerFast.from_pretrained(name)
         nllb = AutoModelForSeq2SeqLM.from_pretrained(name)
-    
-    # tokenizer.src_lang = "kor_Hang"
-    # inputs = tokenizer(text="다만 하반기에도 이같은 기조가 이어질 지에 대해서는 의견이 분분하다.", return_tensors="pt")
-    # translated_tokens = nllb.generate(
-    #     **inputs, forced_bos_token_id=tokenizer.lang_code_to_id["eng_Latn"]
-    # )
 
-    def test_translations(model, data, batch_size=25):
-      batch_size = 50
-      test_df_small = data[:1000]
-      batches = [test_df_small.iloc[i:i + batch_size] for i in range(0, len(test_df_small), batch_size)]
+    def test_translations(model, data, batch_size=25, num_beams=4, small=False):
+        if small:
+            data = data[:len(data)//10]
+        korean_src = data['한국어'].tolist()
+        batches = [korean_src[i:i + batch_size] for i in range(0, len(korean_src), batch_size)]
 
-      english_translations = []
-      for df_batch in tqdm(batches):
-          inputs = tokenizer(text=df_batch['한국어'].tolist(), return_tensors="pt", padding=True, truncation=True)
-          translated_tokens = model.generate(
-              **inputs, forced_bos_token_id=tokenizer.lang_code_to_id["eng_Latn"]
-          )
-          translated_sentences = tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)
-          english_translations += translated_sentences
-      english_translations_split = [t.split() for t in english_translations]
-      english_sentences = test_df['영어'].tolist()[:1000]
-      english_sentences_split = [[s.split()] for s in english_sentences]
-      bleu_score = corpus_bleu(english_sentences_split, english_translations_split)
-      print('Model BLEU score: %.3f', bleu_score)
-      return bleu_score
+        english_translations = []
+        for df_batch in tqdm(batches):
+            tokenizer.src_lang = 'kor_Hang'
+            tokenizer.tgt_lang = 'eng_Latn'
+            inputs = tokenizer(text=df_batch['한국어'].tolist(), return_tensors="pt", padding=True, truncation=True)
+            model.eval()
+            translated_tokens = model.generate(
+                **inputs.to(model.device), 
+                forced_bos_token_id=tokenizer.lang_code_to_id["eng_Latn"],
+                num_beams=4
+            )
+            translated_sentences = tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)
+            english_translations += translated_sentences
+        english_translations_split = [t.split() for t in english_translations]
+        english_gt_split = [[s.split()] for s in data['영어'].tolist()]
+        bleu_score = corpus_bleu(english_gt_split, english_translations_split)
+        print('Model BLEU score: %.3f', bleu_score)
 
-    from transformers.optimization import Adafactor
-    from transformers import get_constant_schedule_with_warmup
+        with open(args.out_file, 'w') as of:
+            for e in english_translations:
+                of.write(e)
+
+        return bleu_score
 
     optimizer = Adafactor(
         [p for p in nllb.parameters() if p.requires_grad],
         scale_parameter=False,
         relative_step=False,
-        lr=1e-4,
+        lr=args.lr,
         clip_threshold=1.0,
         weight_decay=1e-3,
     )
     scheduler = get_constant_schedule_with_warmup(optimizer, num_warmup_steps=100)
 
     data_train = train_df.copy()
+    
+    if args.curriculum:
+      data_train = sort_dataset_by_size(data_train)
+      
     data_train = data_train.filter(['한국어', '영어'], axis=1)
 
-    batch_size = 50
+    batch_size = args.batch_size
     losses = []
     bleu_scores = []
     nllb_new = deepcopy(nllb)
@@ -136,6 +166,9 @@ def main():
             loss.backward()
             losses.append(loss.item())
 
+            if i % args.print_every == 0:
+                print(loss.item())
+
             optimizer.step()
             optimizer.zero_grad(set_to_none=True)
             scheduler.step()
@@ -146,27 +179,21 @@ def main():
             clear_mem()
             continue
 
-        if i % args.checkpoint_every == 0:
-            print(i, np.mean(losses[-1000:]))
+        if i % args.plot_every == 0:
+            print(f'iteration: {i} mean loss: {np.mean(losses[-args.plot_every:])}')
             score = test_translations(nllb_new, val_df)
+            print(f'current bleu score is: {score}')
             bleu_scores.append(score)
+            plot_loss_and_bleu(losses, bleu_scores, i)
 
         if i % args.checkpoint_every == 0 and i > 0:
             nllb_new.save_pretrained(args.save_path)
             tokenizer.save_pretrained(args.save_path)
-    test_translations(nllb_new, test_df)
+
+        
+    final_bleu = test_translations(nllb_new, test_df)
+    print(f'final bleu score is: {final_bleu}')
     
-    plt.plot(losses)
-    plt.xlabel("Iterations")
-    plt.ylabel("Loss")
-    plt.show()
-    plt.savefig('train_loss.png')
-    
-    plt.plot(bleu_scores)
-    plt.xlabel("10k Iterations")
-    plt.ylabel("Bleu Score on Test")
-    plt.show()
-    plt.savefig('bleu_score.png')
 
 if __name__ == '__main__':
     main()
